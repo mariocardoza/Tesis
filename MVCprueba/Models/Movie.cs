6d02f4767c28ccdc1e3e5c2c4bde93eed88b276e
Mario <mariokr.rocker@gmail.com>
2018-03-03 10:29:43 -0600

    primer comit

---
+++ MVCprueba/Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCprueba.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+
+    public class MovieDBContext : DbContext  
+    {
+        public MovieDBContext() : base("DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MovieDBContext, MVCprueba.Migrations.Configuration>());
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
